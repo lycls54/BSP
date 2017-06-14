8783b0b6a81fd0a986a25c03fae8efd1c9eb7781
NoobieMaser <NoobieMaser@users.noreply.github.com>
2017-06-14 11:16:32 +0200

    Create PageTable.java
    Aufgabe 4 noch Fixxen

--- A4/src/de/haw/bsp/osbsp/PageTable.java
+++ A4/src/de/haw/bsp/osbsp/PageTable.java
@@ -159,7 +159,7 @@ public class PageTable {
 		// TODO
 		int rnd = (int) Math.ceil(Math.random() * pteRAMlist.size() - 1);
 		os.testOut(String.format("Prozess %d: Random-Algorithmus hat pte ausgew�hlt: %d", pid, rnd));
-		return pteRAMlist.set(rnd, newPte);
+		return pteRAMlist.set(rnd, newPte);// Alte Seite zurückgeben
 	}
 
 	// ----------------------- Hilfsmethode --------------------------------
